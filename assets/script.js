87886b8e2039e4d0ca90c0b89b97be2402fc732f
jacob maddaus <jakemaddaus@outlook.com>
2024-01-08 15:11:57 -0600

    added the JS to retrieve data from local storage, as well as comments to JS page

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,6 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
- 
 
-//  Displays todays date and time and updates every second.
+//  Displays todays date and time.
 
 var todayDate = dayjs().format(" MMM DD, YYYY h:mm:ss a");
 $("#currentDay").html(todayDate);
@@ -40,10 +19,11 @@ $(document).ready(function () {
 
 
 
-//this function is to check the hour block against the current time and to add classes dependent on if the hour block is in the past, present, or future.
 function hourTracker() {
 
   var currentHour = dayjs().hour();
+//this is to check the hour block against the current time and to add classes dependent on if the hour block is in the past, present, or future.
+// the different classes change the background color base on past, present, or future.
 
   $(".time-block").each(function() {
     var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
@@ -68,21 +48,19 @@ function hourTracker() {
 
 
 }
-
-// grabbing items from local storage.
-
+// this is grabbing the stored items back out of local storage.
+$("#hour8 .description").val(localStorage.getItem("hour8"));
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
 $("#hour11 .description").val(localStorage.getItem("hour11"));
 $("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour1 .description").val(localStorage.getItem("hour1"));
-$("#hour2 .description").val(localStorage.getItem("hour2"));
-$("#hour3 .description").val(localStorage.getItem("hour3"));
-$("#hour4 .description").val(localStorage.getItem("hour4"));
-$("#hour5 .description").val(localStorage.getItem("hour5"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
 hourTracker();
-
 
 
 });
